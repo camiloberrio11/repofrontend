5c8a2e960778b72f6b5f9b05cd700a9251f176dc
Juan Camilo Berrio - Ceiba Software <camilo.berrio@ceiba.com.co>
2021-10-12 09:48:22 -0500

    change table in history component

--- src/app/full-layout/history/history.component.ts
+++ src/app/full-layout/history/history.component.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { usersListData } from './users-list.data';
-
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { usersListData } from "./users-list.data";
+import { RequestPqrsPopulate } from "../../shared/models/RequestPqrs";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import { PqrApiService } from "app/shared/services/pqr-api.service";
+import { NgxSpinnerService } from "ngx-spinner";
+import { ToastrService } from "ngx-toastr";
+import { BodyRequestByStatus } from "../../shared/models/RequestPqrs";
 @Component({
-  selector: 'app-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss']
+  selector: "app-history",
+  templateUrl: "./history.component.html",
+  styleUrls: ["./history.component.scss"],
 })
 export class HistoryComponent implements OnInit {
-  constructor() { }
+  listHistoryPqrs: RequestPqrsPopulate[] = [];
+  closeResult: string;
+
+  constructor(
+    private modalService: NgbModal,
+    private pqrApi: PqrApiService,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.getHistoryPqrs();
   }
 
-  viewItem(): void {
-    console.log('Viendo')
+  open(content: any, item: RequestPqrsPopulate) {
+    this.modalService.open(content).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
   }
 
+  private getHistoryPqrs(): void {
+    this.spinner.show();
+    const body: BodyRequestByStatus = { page: 1, limit: 10, closed: true };
+    this.pqrApi.getRequestByStatus(body).subscribe(st => {
+      this.listHistoryPqrs = st?.data;
+      this.spinner.hide();
+    }, err => {
+      this.spinner.hide();
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
--- src/app/full-layout/history/history.module.ts
+++ src/app/full-layout/history/history.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HistoryComponent } from './history.component';
 import { HistoryRoutingModule } from './history-routing.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [HistoryComponent],
   imports: [
     CommonModule,
     HistoryRoutingModule,
+    NgxSpinnerModule
   ]
 })
 export class HistoryModule { }
